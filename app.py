5069b8c58d75bcb113a85b7a633c25a969cea87e
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2024-01-16 17:57:18 +0900

    add

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import serverless_wsgi
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from config import Config
-from resources.CompareFacesResource import CompareFacesResource
+from resources.compareFacesResource import CompareFacesResource
+
 
 # 로그아웃 관련된 임포트문
 # from resources.user import jwt_blocklist
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 # 환경변수 셋팅
 app.config.from_object(Config)
+
 # JWT 매니저 초기화
 jwt=JWTManager(app)
 
@@ -21,8 +22,6 @@ api = Api(app)
 # 경로와 리소스를 연결한다.
 api.add_resource(CompareFacesResource,'/upload')
 
-# def handler(event, context) :
-#     return serverless_wsgi.handle_request(app, event, context)
 
 if __name__ == '__main__' :
     app.run()
--- resources/CompareFacesResource.py
+++ resources/CompareFacesResource.py
@@ -1,13 +1,12 @@
+from datetime import datetime
 from flask import request
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, get_jwt
 from flask_restful import Resource
 from config import Config
-from mysql.connector import Error
 
-from datetime import datetime
+
 from io import BytesIO
-import io
 import boto3
+from PIL import Image, ImageDraw, ImageFont
 
 class CompareFacesResource(Resource) :
     def post(self) :               
@@ -17,11 +16,53 @@ class CompareFacesResource(Resource) :
 
         if source_file is None or target_file is None :
             return {"error" : '파일을 업로드 하세요'}, 400
-        
-        face_matches = self.compare_faces(source_file.read(), target_file.read())
+                
+        data = self.compare_faces(source_file.read(), target_file.read())
 
-        return {'result' : 'success', "items" : face_matches,}, 200
+        face_matches = data['FaceMatches']
+        face_unmatches = data['UnmatchedFaces']
+
+        for faceMatch in data['FaceMatches']:
+            position = faceMatch['Face']['BoundingBox']
+            similarity = str(faceMatch['Similarity'])
+         
+
+        # 얼굴 비교 표시하기
+        image = Image.open(target_file)
+        font = ImageFont.truetype('NanumGothic', size=30)
+        draw = ImageDraw.Draw(image)
+        w, h = image.size
+        
+        if {face_matches == True}:
+            name = format(similarity, ".6s") + '%'
+            x0 = int(position['Left'] * w)
+            y0 = int(position['Top'] * h)
+            x1 = x0 + int(position['Width'] * w)
+            y1 = y0 + int(position['Height'] * h)
+            draw.rectangle([x0, y0, x1, y1], outline=(255, 0, 0), width=3)
+            draw.text((x0, y1), name, font=font, fill=(255, 0, 0))
+
+        for face_unmatches in data['UnmatchedFaces']:
+            x0 = int(face_unmatches['BoundingBox']['Left'] * w)
+            y0 = int(face_unmatches['BoundingBox']['Top'] * h)
+            x1 = x0 + int(face_unmatches['BoundingBox']['Width'] * w)
+            y1 = y0 + int(face_unmatches['BoundingBox']['Height'] * h)
+            draw.rectangle([x0, y0, x1, y1], outline='yellow', width=2)
+            
+        
+        # 사진 파일 저장
+        current_time = datetime.now()
+        new_file_name = current_time.isoformat().replace(':', '_') + '.jpg'  
+        
+        image.save(new_file_name)
+ 
+        return {'result' : 'success', 
+                "matchItems" : face_matches,
+                "unMatchItems" : face_unmatches
+                }, 200
     
+
+    # 얼굴 비교
     def compare_faces(self, sourceFile, targetFile):
 
         client = boto3.client('rekognition',
@@ -39,12 +80,14 @@ class CompareFacesResource(Resource) :
         for faceMatch in response['FaceMatches']:
             position = faceMatch['Face']['BoundingBox']
             similarity = str(faceMatch['Similarity'])
+            
             print('감지된 얼굴에서 ' +
                 str(position['Left']) + ' ' +
                 str(position['Top']) +
-                ' matches with ' + similarity + '% confidence')
+                ' 위치의 얼굴이 ' + similarity + '% 유사도')
 
         imageSource.close()
         imageTarget.close()
 
-        return response['FaceMatches']
+        return response
+    
